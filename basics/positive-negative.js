fc11addde5dd73439a2cd9588e6e93cebbb42787
Harsh Tomar <harshpardeshi2022@gmail.com>
2026-03-29 19:47:21 +0530

    Added positive negative number program

---
+++ basics/positive-negative.js
@@ -0,0 +1,13 @@
+// Check if number is positive, negative, or zero
+// Input: 0
+// Output: Zero
+
+let num = 0;
+
+if (num > 0) {
+    console.log("Positive");
+} else if (num === 0) {
+    console.log("Zero");
+} else {
+    console.log("Negative");
+}
